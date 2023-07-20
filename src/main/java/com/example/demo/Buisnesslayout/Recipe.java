883afca5de0ff9f4915a2c6e9e5adb7f5bc4ab78
meriem al ahmadi hammou <mariama.alahmadi95@gmail.com>
2023-07-20 22:38:08 +0200

    adding more constraints to be validated

--- src/main/java/com/example/demo/Buisnesslayout/Recipe.java
+++ src/main/java/com/example/demo/Buisnesslayout/Recipe.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import java.util.List;
 @Entity
@@ -22,21 +24,23 @@ public class Recipe {
     @Column(name = "recipe_id")
     private long id;
 
+    @NotNull
+    @NonNull
     @NotBlank
-    @Column
     private String name;
 
+    @NotNull
+    @NonNull
     @NotBlank
-    @Column
     private String description;
 
     @Size(min = 1)
-    @Column
+    @NotNull
     @ElementCollection
     private List<String> ingredients;
 
     @Size(min = 1)
-    @Column
+    @NotNull
     @ElementCollection
     private List<String> directions;
 }
