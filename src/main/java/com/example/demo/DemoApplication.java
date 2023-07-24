3626aa0e9d24bc93d691981b4fb0035cf0889ebf
meriem al ahmadi hammou <mariama.alahmadi95@gmail.com>
2023-07-24 12:09:17 +0200

    example about encryption

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,14 +11,14 @@ import org.springframework.security.crypto.keygen.KeyGenerators;
 
 
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner {
-
+public class DemoApplication //implements CommandLineRunner {
+{
 	public static void main(String[] args) {
 
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
-	@Bean
+//for encryption
+/*	@Bean
 	public BytesEncryptor aesBytesEncryptor() {
 		String password = "hackme"; // should be kept in a secure place and not be shared
 		String salt = "8560b4f4b3"; // should be hex-encoded with even number of chars
@@ -36,7 +36,7 @@ public class DemoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-/*		BytesEncryptor bytesEncryptor = aesBytesEncryptor();
+*//*		BytesEncryptor bytesEncryptor = aesBytesEncryptor();
 
 		byte[] inputData = {104, 121, 112, 101, 114, 115, 107, 105, 108, 108};
 		byte[] encryptedData = bytesEncryptor.encrypt(inputData);
@@ -44,7 +44,7 @@ public class DemoApplication implements CommandLineRunner {
 
 		System.out.println("Input data: " + new String(inputData));
 		System.out.println("Encrypted data: " + new String(encryptedData));
-		System.out.println("Decrypted data: " + new String(decryptedData));*/
+		System.out.println("Decrypted data: " + new String(decryptedData));*//*
 
 		//for text:
 		TextEncryptor textEncryptor = hexEncodingTextEncryptor();
@@ -55,6 +55,6 @@ public class DemoApplication implements CommandLineRunner {
 		System.out.println("Input data: " + inputData);
 		System.out.println("Encrypted data: " + encryptedData);
 		System.out.println("Decrypted data: " + decryptedData);
-	}
+	}*/
 }
 
